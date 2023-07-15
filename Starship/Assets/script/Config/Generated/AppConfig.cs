b7de18396dffc2a7d728a5c648dedf66f54abc11
zkip <zkiplan@qq.com>
2023-07-16 01:25:08 +0800

    完善中文翻译和其它语种部分翻译

--- Starship/Assets/script/Config/Generated/AppConfig.cs
+++ Starship/Assets/script/Config/Generated/AppConfig.cs
@@ -1,8 +1,8 @@
 public static class AppConfig
 {
     public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
-    public const string version = "1.9.6";
+    public const string version = "1.9.6.1";
     public const int versionCode = 109600;
-    public const int buildNumber = 1426;
+    public const int buildNumber = 1427;
 }
 
