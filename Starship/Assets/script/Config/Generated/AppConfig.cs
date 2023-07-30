f2fa4317818954874a11e23faba5a8e553bb8e4a
zkip <zkiplan@qq.com>
2023-07-30 20:32:35 +0800

    verision 1.9.6.4

--- Starship/Assets/script/Config/Generated/AppConfig.cs
+++ Starship/Assets/script/Config/Generated/AppConfig.cs
@@ -1,8 +1,8 @@
 public static class AppConfig
 {
     public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
-    public const string version = "1.9.6.2";
-    public const int versionCode = 109620;
-    public const int buildNumber = 1429;
+    public const string version = "1.9.6.4";
+    public const int versionCode = 109640;
+    public const int buildNumber = 1438;
 }
 
